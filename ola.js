3405e3da78432ff283e4d531114fab3a9e6f90e8
cadumd <cadu.md2021@gmail.com>
2024-09-11 14:53:34 -0300

    huuih

---
+++ ola.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require("fs")
+
+
+const server = http.createServer((pedido, resposta) => {
+    const url = pedido.url
+    console.log(url)
+    switch (url) {
+        case '/treino':
+            pedido.on('data', (dados) => {
+                let dadosDoBanco = JSON.parse(fs.readFileSync('./mensage.json'))
+                let novosDados = JSON.parse(dados)
+
+                dadosDoBanco.push(novosDados)
+
+                fs.writeFileSync('mensage.json', JSON.stringify(dadosDoBanco));
+            })
+            break;
+    }
+})
+
+
+server.listen(3000, /*"ipconfig"*/)

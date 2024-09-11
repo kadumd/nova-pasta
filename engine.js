1a0aefdbade1ca48a0f5f8ae0116fc8025a895ca
carlos <156134788+kadumd@users.noreply.github.com>
2024-09-11 11:14:34 -0300

    Add files via upload

---
+++ engine.js
@@ -0,0 +1,22 @@
+const chat = document.querySelector(".chat")
+const oi = await fetch("./mensage.json")
+const servidor = await oi.json()
+
+servidor.forEach(dados => {
+    console.log(dados)
+    const card = document.createElement("p")
+    card.classList.add("card")
+    card.textContent = dados.nome
+
+    chat.appendChild(card)
+});
+
+/*botao.addEventListener("click", ()=>{
+            const nome = document.querySelector('#nome').value
+            const caracteristicas = document.querySelector('#caracteristicas').value
+
+            fetch('/treino',{
+                method: "POST",
+                body: JSON.stringify({nome:nome, caracteristicas:caracteristicas})
+            })
+        })*/

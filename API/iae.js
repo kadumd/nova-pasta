fa9f4206c58206133e08c9f7a1add72a03332271
cadumd <cadu.md2021@gmail.com>
2024-09-15 19:51:06 -0300

    Uhi

--- API/iae.js
+++ API/iae.js
@@ -1,6 +1,16 @@
 export default function handler(pedido, resposta) {
     if (pedido.url === '/treino') {
-        console.log("oi")
+        pedido.on('data', (dados) => {
+            const fs = require("fs")
+            //parece que o erro está por aqui "pq o codigo para de ser executado aqui"
+            const dadosDoBanco = JSON.parse(fs.readFileSync('./mensage.json'))
+            //----------------
+            const novosDados = JSON.parse(dados)
+
+            dadosDoBanco.push(novosDados)
+
+            fs.writeFileSync('mensage.json', JSON.stringify(dadosDoBanco));
+        })
     } else {
         resposta.status(405).json({ message: 'Método não permitido' });
     }
